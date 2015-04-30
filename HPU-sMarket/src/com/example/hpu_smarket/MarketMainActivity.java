f1bc260c2a0b355abc87d7c3b85ff4aee8a7f247
taxuexumei <1263347614@qq.com>
2015-04-30 14:03:03 +0800

    second

---
+++ HPU-sMarket/src/com/example/hpu_smarket/MarketMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hpu_smarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MarketMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_main);
+    }
+}
---
+++ ceshi/src/com/example/ceshi/ChangeView.java
@@ -0,0 +1,20 @@
+package com.example.ceshi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ChangeView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		/*super.onCreate(savedInstanceState);
+		Button button = (Button)findViewById(R.id.buttonchange);
+		
+		
+		Intent intent = (Intent)setContentView(R.layout.);*/
+		
+	}
+
+}
